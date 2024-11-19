b4fe6c3f67bcd3b81cc91d8d79351e99f3dd765a
miaschwarzmaier <mschwarzmaier@udesa.edu.ar>
2024-11-19 14:36:55 -0300

    principio Home y New Post

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,48 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { View, Text, FlatList } from 'react-native';
+import Card from '../components/Card';
 
-export default class Home extends Component {
+import { db } from '../firebase/config';
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: null,
+    }
+  }
+
+  componentDidMount() {
+    db.collection("posts").onSnapshot(snapshot => { // SELECT * FROM posts;
+      const posts = []
+      snapshot.forEach(doc => {
+        posts.push(doc.data())
+      })
+      this.setState({ posts: posts })
+    })
+  }
+
   render() {
     return (
-      <div>
-        
-      </div>
+      <View>
+        {
+          this.state.posts === null ?
+          <Text>Cargando...</Text> : 
+          <View>
+            { 
+              this.state.posts.length === 0 ?
+              <Text>No hay posts</Text> :
+              <FlatList
+                data={this.state.posts}
+                renderItem={({ item }) => <Card post={item} />}
+                keyExtractor={item => item.id}
+              />
+            }
+          </View>
+        }
+      </View>
     )
   }
 }
+
+export default Home;
--- src/screens/NewPost.js
+++ src/screens/NewPost.js
@@ -1,12 +1,30 @@
-//formulario de creacion del posteo 
-import React, { Component } from 'react'
+// Formulario de creación del posteo
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { db } from '../firebase/config';
 
-export default class NewPost extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+class NewPost extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+
+    createPost() {
+        db.collection("posts").add({
+            title: "Titulo del post",
+            description: "Descripción del post",
+            image: "https://via.placeholder.com/150",
+            owner: ""
+        })
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>NewPost</Text>
+            </View>
+        )
+    }
 }
+
+export default NewPost;
