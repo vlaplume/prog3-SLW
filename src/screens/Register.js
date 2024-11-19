dd1f0f9abf74d38632ccf25656443137f07da53f
vlaplume <vlaplume@udesa.edu.ar>
2024-11-18 22:53:54 -0300

    Registracion

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,11 +1,110 @@
-import React, { Component } from 'react'
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from "react";
+import { Text, View, TouchableOpacity, TextInput, StyleSheet } from 'react-native';
+import { auth, db } from "../firebase/config";
 
 export default class Register extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: "",
+      password: "",
+      username: "",
+      error: "",
+    }
+  }
+
+  componentDidMount() {
+    if (auth.currentUser !== null) {
+      this.props.navigation.navigate("HomeMenu")
+    }
+  }
+
+  registrarUsuario(email, password, username) {
+    if (email === "" || password === "" || username === "") {
+      this.setState({
+        error : "Debe completar todos los campos del registro."
+      })
+      return 
+    }
+    auth.createUserWithEmailAndPassword(email, password)
+      .then(res => {
+        db.collection("users").add({ Owner: email, Username: username, })
+          .then(() => {
+            this.setState({
+              email: "",
+              password: "",
+              username: "",
+              error: "",
+            })
+          })
+          .catch(err => {
+            this.setState({
+              error: err.message
+            })
+          })
+
+      })
+  }
+
   render() {
     return (
-      <div>
-        
-      </div>
+      <View style={styles.container}>
+        <Text style={styles.heading}> Formulario de Registro</Text>
+
+        <TextInput style={styles.input}
+          keyboardType='default'
+          placeholder='Ingrse su nombre de usuario'
+          onChangeText={text => this.setState({ username: text })}
+          value={this.state.username} />
+
+
+        <TextInput style={styles.input}
+          keyboardType="email-address"
+          placeholder="Ingrese su email"
+          onChangeText={text => this.setState({ email: text })}
+          value={this.state.email} />
+
+        <TextInput style={styles.input}
+          keyboardType="password"
+          placeholder="Ingrese su constraseña"
+          onChangeText={text => this.setState({ password: text })}
+          value={this.state.password} />
+
+        <TouchableOpacity onPress={() => this.registrarUsuario(this.state.email, this.state.password, this.state.username)}>
+          <Text>Registrarse</Text>
+        </TouchableOpacity>
+
+        {
+          this.state.error ?
+          <Text style={styles.error}>{this.state.error}</Text> :
+          null
+        }
+
+        <Text>Si ya estás registrado andá a <TouchableOpacity onPress={() => this.props.navigation.navigate("Login")}>Login</TouchableOpacity>
+        </Text>
+
+        <StatusBar style="auto" />
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  heading: {
+
+  },
+  input: {
+    border: "1px solid #000000",
+    borderRadius : "8pt"
+  },
+  error: {
+    color: "rgb(255,0,0)",
+  }
+});
