f010ed5c6de01f1056892e9621e05c3662021017
nwilliss <nwillis@udesa.edu.ar>
2024-11-22 18:36:26 -0300

    profile y search arrancados

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { auth } from "../firebase/config";
 
 export default class Profile extends Component {
   render() {
     return (
       <div>
-        
+        <TouchableOpacity onPress={() => auth.signOut()}>Cerrar sesión</TouchableOpacity>
       </div>
     )
   }
--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 
-export default class Search extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>Search</Text>
+            </View>
+        )
+    }
 }
+
+export default Search;
