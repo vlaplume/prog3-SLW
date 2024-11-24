55345ef446b17fbc1c8774c91e3a3426b26600bf
nwilliss <nwillis@udesa.edu.ar>
2024-11-24 16:20:52 -0300

    search.js screen

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,16 +1,68 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, FlatList } from 'react-native';
 
 class Search extends Component {
     constructor(props) {
         super(props);
-        this.state = {  };
+        this.state = {
+            allUsers : [],
+            users : null,
+            username : ''
+          };
     }
-
+    componentDidMount() {
+        db.collection("users").onSnapshot(doc => {
+          let allUsers = [];
+          doc.forEach(user => {
+            console.log(user.data())
+            allUsers.push(user)
+          })
+          this.setState({
+            allUsers : allUsers
+          })
+        })
+      }
+    
+      buscarUser() {
+        let users = this.state.allUsers.filter(user => {
+          return user.data().Username.includes(this.state.username)
+        })
+          this.setState({
+            users : users
+          })
+      }
+    
+      componentDidUpdate(prevProps, prevState) {
+        if (prevState.username !== this.state.username) {
+          console.log("Buscando...")
+          this.buscarUser();
+        }
+      }
     render() {
         return (
             <View>
-                <Text>Search</Text>
+                <Text>Buscar por usuario</Text>
+        <TextInput
+          style=''
+          keyboardType='default'
+          placeholder='Ingrse el nombre de usuario'
+          onChangeText={text => this.setState({ username: text })}
+          value={this.state.username}
+        />
+        <Text>Resultados de búsqueda</Text>
+        {
+          this.state.username !== '' ?
+            this.state.users === null ?
+              <Text>Cargando...</Text> :
+              this.state.users.length === 0 ?
+                <Text>No hay usuarios</Text> :
+                <FlatList
+                  data={this.state.users}
+                  renderItem={({ item }) => <Text>{item.data().Username}</Text>}
+                  keyExtractor={item => item.id}
+                />
+            : null
+        }
             </View>
         )
     }
